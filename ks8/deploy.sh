0a602323832ed700683d38ccc747d8a18c10d8b4
Viet Nguyen <vnguyen@redhat.com>
2015-05-27 15:57:59 -0400

    Use v1beta3 and RC

--- ks8/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Deleting pods..."
-kubectl delete -f pod.json
-sleep 10s
-echo "Deploying..."
-kubectl create -f pod.json
--- ks8/runonce.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Deleting..."
-kubectl delete -f hawk-app-service.json
-kubectl delete -f test-results-service.json
-echo "Creating..."
-kubectl create -f hawk-app-service.json
-kubectl create -f test-results-service.json
