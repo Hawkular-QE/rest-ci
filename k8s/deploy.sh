7e9bf2d470676ba2eee537aec0486998486714e1
Viet Nguyen <vnguyen@redhat.com>
2015-06-24 03:46:57 -0400

    Simpifly and improve deployment script

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-if [ -z ${HAWKULAR_ENDPOINT} ]; then
-   echo "Missing HAWKULAR_ENDPOINT env"
+if [ -z ${APP_LABEL} ]; then
+   echo "Missing APP_LABEL env.  Did you source env file?"
    exit 1
 fi
+echo "*** APP_LABEL: ${APP_LABEL} ***"
+if [ "$1" == "reset" ]; then
+   echo "Resetting..."
+   kubectl delete -f ${KUBE_RC}
+   kubectl delete pods -l "${APP_LABEL}"
+   kubectl delete service -l "${APP_LABEL}"
+   echo "Creating replication controller..."
+   kubectl --validate create -f ${KUBE_RC}
 
-: ${APP_LABEL:=name=hawkular, env=snapshot}
-echo "Deleting pods... label: ${APP_LABEL}"
-
-RC_ID=$(kubectl get rc -l "${APP_LABEL}" -o template --template='{{(index .items 0).metadata.name}}')
-kubectl resize --replicas=0 rc ${RC_ID}
+   sleep 5s
+   RC_ID=$(kubectl get rc -l "${APP_LABEL}" -o template --template='{{(index .items 0).metadata.name}}')
+   echo "Creating service for rc ${RC_ID}..."
+   kubectl expose rc ${RC_ID} --port=${PUBLIC_PORT} --target-port=8080  --public-ip=${NODE_IP}
+   kubectl label --overwrite services ${RC_ID} ${APP_LABEL//,/ }
+else
+   # preconditions: Pod and RC have been running already from previous run
+   echo "Deleting pods..."
+   RC_ID=$(kubectl get rc -l "${APP_LABEL}" -o template --template='{{(index .items 0).metadata.name}}')
+   kubectl resize --replicas=0 rc ${RC_ID}
+fi
 
 sleep 5s
 
@@ -30,5 +44,5 @@ LOG_PID=$!
 status=$?
 
 kill -9 ${LOG_PID}
-
+unset APP_LABEL
 exit $status
--- k8s/snapshot-env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export APP_LABEL="name=hawkular, env=snapshot"
-export HAWKULAR_ENDPOINT="http://209.132.179.82:18080/"
